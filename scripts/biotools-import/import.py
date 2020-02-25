249bd637fb8e8a72d4610b452780abb056cabd78
Hervé Ménager <herve.menager@gmail.com>
2020-02-25 19:09:13 +0100

    first functional version of the bio.tools import tool (beta)

--- scripts/biotools-import/import.py
+++ scripts/biotools-import/import.py
@@ -1,48 +1,64 @@
-import requests
 import json
 import os
 import sys
+import glob
 import argparse
 
+import requests
 
-def main(filters=None, limit=-1, dump=False):
-    
+
+def clean():
+    for data_file in glob.glob(r"../../data/*/*.json"):
+        filename_ext = os.path.basename(data_file).split(".")
+        if len(filename_ext) == 2 and filename_ext[1] == "json":
+            os.remove(data_file)
+
+
+def retrieve(filters=None):
     """
-    Go through all bio.tools entries and produce an RDF graph representation (BioSchemas / JSON-LD).  
-    :param limit: an integer value specifying the max number of entries to be crawled, -1 by default, means no limit.
+    Go through bio.tools entries using its API and save the JSON files
+    in the right folders
     """
-    
+
     i = 1
     nb_tools = 1
     has_next_page = True
     filters = filters or {}
-    while has_next_page :
-        parameters = {**filters, **{'page':i}}
+    while has_next_page:
+        parameters = {**filters, **{"page": i}}
         response = requests.get(
-            'https://bio.tools/api/tool/',
+            "https://bio.tools/api/tool/",
             params=parameters,
-            headers={'Accept': 'application/json'},
+            headers={"Accept": "application/json"},
         )
         try:
             entry = response.json()
         except JSONDecodeError as e:
-            print("Json decode error for " + str(req.data.decode('utf-8')))
+            print("Json decode error for " + str(req.data.decode("utf-8")))
             break
-        has_next_page = (entry['next'] != None)
+        has_next_page = entry["next"] != None
 
-        for tool in entry['list']:
-            tool_id = tool['biotoolsID']
-            print(tool_id)
+        for tool in entry["list"]:
+            tool_id = tool["biotoolsID"]
+            tpe_id = tool_id.lower()
+            directory = os.path.join("..", "..", "data", tpe_id)
+            if not os.path.isdir(directory):
+                os.mkdir(directory)
+            with open(os.path.join(directory, tpe_id + ".json"), "w") as write_file:
+                json.dump(tool, write_file)
             nb_tools += 1
+            print(f"import tool #{nb_tools}: {tool_id} in folder {directory}")
         i += 1
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='biotools to bioschemas bot script')
-    parser.add_argument('collection', type=str, default='*', nargs='?',
-                    help='collection name filter')
-    args = parser.parse_args()
-    if args.collection=='*':
-        main(limit=-1, dump=True)
-    else:
-        main(filters={'collection':args.collection}, limit=-1, dump=True)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="biotools to bioschemas bot script")
+    parser.add_argument(
+        "collection", type=str, default="*", nargs="?", help="collection name filter"
+    )
+    args = parser.parse_args()
+    clean()
+    if args.collection == "*":
+        retrieve()
+    else:
+        retrieve(filters={"collection": args.collection})
