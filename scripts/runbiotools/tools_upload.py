745b8f75ae7df4c3bcb7974b3fc8a70f2ad52d3f
Hervé Ménager <herve.menager@gmail.com>
2020-07-22 18:16:19 +0200

    remove obsolete commented part from tools_upload.py

--- scripts/runbiotools/tools_upload.py
+++ scripts/runbiotools/tools_upload.py
@@ -7,31 +7,6 @@ import argparse
 
 import requests
 
-"""
-# curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d '{"username":"uploadbot", "password1":"botbotbot", "password2":"botbotbot", "email":"uploadbot@example.com"}' "http://localhost:8000/api/rest-auth/registration/"
-tmpuser = 'uploadbot' + str(datetime.timestamp(datetime.now()))
-payload_dict = {'username': tmpuser, 'password1': 'botbotbot', 'password2': 'botbotbot', 'email': f'{tmpuser}@example.com'}
-headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
-url = "http://localhost:8000/api/rest-auth/registration/"
-response = requests.post(url, headers=headers, json=payload_dict)
-token = response.json()['key']
-headers['Authorization'] = f'Token {token}'
-url = "http://localhost:8000/api/tool/validate/"
-#register tools
-for biotools_json_file in glob.glob('../content/data/*/*.biotools.json'):
-    try:
-        logging.debug(f'uploading {biotools_json_file}...')
-        payload_dict=json.load(open(biotools_json_file))
-        response = requests.post(url, headers=headers, json=payload_dict)
-        response.raise_for_status()
-        logging.debug(response.json())
-        logging.debug(f'done uploading {biotools_json_file}')
-    except requests.exceptions.HTTPError:
-        logging.error(f'error while uploading {biotools_json_file} (status {response.status_code}): {response.text}')
-    except:
-        logging.error(f'error while uploading {biotools_json_file}', exc_info=True)
-"""
-
 HEADERS = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 HOST = 'http://localhost:8000/'
 
