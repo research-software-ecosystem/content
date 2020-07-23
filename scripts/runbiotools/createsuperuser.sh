d0b8e269f04283496327bf40c8d14b5a4148dcb5
Hervé Ménager <herve.menager@gmail.com>
2020-07-23 17:01:43 +0200

    correct execution for createsuperuser in runbiotools

--- scripts/runbiotools/createsuperuser.sh
+++ scripts/runbiotools/createsuperuser.sh
@@ -1,1 +1,2 @@
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('uploadbot', 'uploadbot@example.com', 'botbotbot')" | python manage.py shell
+#!/bin/bash
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('uploadbot', 'uploadbot@example.com', 'botbotbot')" | python3 manage.py shell
