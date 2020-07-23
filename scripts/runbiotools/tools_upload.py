3288b47b785b0fd9b574757e6f7f1425ece78b9b
Hervé Ménager <herve.menager@gmail.com>
2020-07-23 23:05:55 +0200

    improvements to tools_upload in runbiotools
    - count uploaded tools properly
    - remove ontology term labels

--- scripts/runbiotools/tools_upload.py
+++ scripts/runbiotools/tools_upload.py
@@ -30,9 +30,10 @@ def run_upload(token, user):
             logging.debug(f'uploading {biotools_json_file}...')
             payload_dict=json.load(open(biotools_json_file))
             payload_dict["editPermission"]["authors"] = [user]
+            payload_dict = remap(payload_dict, lambda p, k, v: k != 'term')
             response = requests.post(url, headers=headers, json=payload_dict)
             response.raise_for_status()
-            tools_ok = payload_dict["biotoolsID"]
+            tools_ok.append(payload_dict["biotoolsID"])
             logging.debug(response.json())
             logging.debug(f'done uploading {biotools_json_file}')
         except requests.exceptions.HTTPError:
@@ -42,9 +43,10 @@ def run_upload(token, user):
             else:
                 messages = response.text
             logging.error(f'error while uploading {biotools_json_file} (status {response.status_code}): {messages}')
-            tools_ko = payload_dict["biotoolsID"]
+            tools_ko.append(payload_dict["biotoolsID"])
         except:
             logging.error(f'error while uploading {biotools_json_file}', exc_info=True)
+            tools_ko.append(payload_dict["biotoolsID"])
     logging.error('Tools upload finished')
     logging.error(f"Tools OK: {len(tools_ok)}")
     logging.error(f"Tools KO: {len(tools_ko)}")
