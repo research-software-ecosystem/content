6a5266986bc08c616657a6a779b3462b5d713bbd
Dmitry Repchevsky <redmitry@list.ru>
2021-11-25 16:33:40 +0100

    separate commit message and label

--- scripts/git-utils/import-changes.sh
+++ scripts/git-utils/import-changes.sh
@@ -6,14 +6,20 @@ git config --local user.email "tpe-bot@github.com"
 git config --local user.name "Tools Platform Ecosystem bot"
 git add .
 
+if [ -z "$3" ] ; then
+    lbl="$2"
+else
+    lbl="$3"
+fi
+
 if git commit -m "$2 on $(date)"; then
   if $1; then
     git push
   else
-    BRANCH_NAME=$2-$(date +%s%N | cut -b10-19)
+    BRANCH_NAME=$lbl-$(date +%s%N | cut -b10-19)
     git checkout -b $BRANCH_NAME
     git push --set-upstream origin $BRANCH_NAME
-    hub pull-request -m "$2 on $(date)" -l "$2"
+    hub pull-request -m "$2 on $(date)" -l $lbl
   fi
 else
   echo "nothing new to add, exiting"
